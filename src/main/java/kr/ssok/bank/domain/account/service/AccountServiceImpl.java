979ea0d054d8746179df91eb1410b5b00bd9e276
dhku <dhku@naver.com>
2025-04-25 21:12:13 +0900

    fix: 계좌 서비스에 @Transactional 추가 (#11, SCRUM-33)

--- src/main/java/kr/ssok/bank/domain/account/service/AccountServiceImpl.java
+++ src/main/java/kr/ssok/bank/domain/account/service/AccountServiceImpl.java
@@ -13,13 +13,15 @@ import kr.ssok.bank.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
-@RequiredArgsConstructor
 @Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService{
 
     private final UserRepository userRepository;
