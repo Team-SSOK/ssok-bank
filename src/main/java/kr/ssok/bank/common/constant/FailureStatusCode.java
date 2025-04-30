46859cd31b2bc78bee4633a6397aa62a8c681e98
hanzyn09 <ysurkkr12@naver.com>
2025-04-30 15:40:07 +0900

    fix: 계좌 개설 중 good 매칭 안되는 오류 해결 및 AESUtil 추가(#44, SCRUM-140)

--- src/main/java/kr/ssok/bank/common/constant/FailureStatusCode.java
+++ src/main/java/kr/ssok/bank/common/constant/FailureStatusCode.java
@@ -26,7 +26,7 @@ public enum FailureStatusCode implements BaseCode {
     INVALID_TOKEN(HttpStatus.BAD_REQUEST,"AUTH4001", "잘못된 토큰입니다."),
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "AUTH4002", "아이디 또는 비밀번호를 확인해주세요."),
 
-    //계좌 관련 에러
+    // 계좌 관련 에러
     ACCOUNT_NOT_FOUND(HttpStatus.BAD_REQUEST, "ACCOUNT4001", "해당 계좌는 존재하지 않습니다."),
     ACCOUNT_NUMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "ACCOUNT4002", "해당 계좌 번호는 존재하지 않습니다."),
     ACCOUNT_OWNER_CHECK_FAILED(HttpStatus.BAD_REQUEST, "ACCOUNT4003", "해당 계좌 번호에 일치하는 예금주가 없습니다."),
@@ -38,11 +38,11 @@ public enum FailureStatusCode implements BaseCode {
     ACCOUNT_WITHDRAW_LIMIT_REACHED(HttpStatus.BAD_REQUEST, "ACCOUNT4009", "해당 계좌의 출금 한도에 도달하였습니다."),
     ACCOUNT_HISTORY_FAILED(HttpStatus.BAD_REQUEST, "ACCOUNT4010", "거래 내역 조회에 실패하였습니다."),
 
-    //거래 관련 에러
+    // 거래 관련 에러
     TRANSACTION_NOT_EXISTS(HttpStatus.BAD_REQUEST, "TRANSACTION4002", "거래 내역이 존재하지 않습니다."),
     TRANSACTION_CREATE_FAILED(HttpStatus.BAD_REQUEST, "TRANSACTION4003", "거래 내역 생성에 실패하였습니다."),
 
-    //송금 관련 에러
+    // 송금 관련 에러
     TRANSFER_FAILED(HttpStatus.BAD_REQUEST, "TRANSFER4001", "송금에 실패하였습니다."),
     TRANSFER_DEPOSIT_FAILED(HttpStatus.BAD_REQUEST, "TRANSFER4002", "입금 이체에 실패하였습니다."),
     TRANSFER_WITHDRAW_FAILED(HttpStatus.BAD_REQUEST, "TRANSFER4003", "출금 이체에 실패하였습니다."),
@@ -50,7 +50,11 @@ public enum FailureStatusCode implements BaseCode {
     TRANSFER_INTEREST_FAILED(HttpStatus.BAD_REQUEST, "TRANSFER4005", "해당 계좌에 이자 지급을 실패하였습니다."),
 
     //상품 관련 에러
-    GOOD_READ_FAILED(HttpStatus.BAD_REQUEST, "GOOD4001", "상품 목록이 존재하지 않습니다.");
+    GOOD_READ_FAILED(HttpStatus.BAD_REQUEST, "GOOD4001", "상품 목록이 존재하지 않습니다."),
+
+    // 암/복호화 관련 에러
+    AES_ENCRYPT_FAILED(HttpStatus.BAD_REQUEST, "AES4001", "계좌 번호 암호화에 실패했습니다."),
+    AES_DECRYPT_FAILED(HttpStatus.BAD_REQUEST, "AES4002", "계좌 번호 복호화에 실패했습니다..");
 
     private final HttpStatus httpStatus;
     private final String code;
