203e4298eefd781bb5ce6a20d1146ffe696a6647
구동혁 <dhku@naver.com>
2025-05-07 09:37:39 +0900

    fix: 리스너 수정 (#46, SCRUM-137)

--- src/main/java/kr/ssok/bank/domain/transfer/service/TransferServiceImpl.java
+++ src/main/java/kr/ssok/bank/domain/transfer/service/TransferServiceImpl.java
@@ -13,6 +13,7 @@ import kr.ssok.bank.domain.transfer.entity.TransferHistory;
 import kr.ssok.bank.domain.transfer.repository.TransferRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -40,25 +41,64 @@ public class TransferServiceImpl implements TransferService{
      * 요청한 내용을 확인후 응답을 반환합니다.
      * (kafkaListenerReplyContainerFactory 사용)
      *
-     * @param request       DTO 객체
-     * @param key           식별자 키
-     * @param replyTopic    응답해야하는 토픽
+     * @param record        레코드
+     * @param replyTopic    응답을 보내는 토픽
      * @param correlationId 상관 ID
+     * @param cmd           통신 프로토콜
      * @return
      */
     @KafkaListener(topics = "${spring.kafka.request-topic}", groupId = "request-server-group", containerFactory = "kafkaListenerReplyContainerFactory")
-    @SendTo // 응답은 헤더에 지정된 reply topic으로 전송됨
-    public Object handleTransferRequest(Object request,
-                                                  @Header(KafkaHeaders.RECEIVED_KEY) String key,
-                                                  @Header(KafkaHeaders.REPLY_TOPIC) byte[] replyTopic,
-                                                  @Header(KafkaHeaders.CORRELATION_ID) byte[] correlationId) {
+    @SendTo // 응답은 헤더에 지정된 replyTopic으로 전송됨
+    public Object handleTransferRequest(ConsumerRecord<String, Object> record,
+                                        @Header(KafkaHeaders.REPLY_TOPIC) String replyTopic,
+                                        @Header(KafkaHeaders.CORRELATION_ID) String correlationId,
+                                        @Header(value = "CMD", required = false) String cmd) {
 
-        log.info("Received transfer request in bank service: {}", request);
-        log.info("Correlation ID: {}", new String(correlationId));
+        log.info("Received TransferRequest in bank service: {}", record.value());
+        log.info("Received CMD: {}", cmd);
+        log.info("Correlation ID: {}", correlationId);
         log.info("Reply topic: {}", replyTopic);
-        log.info("Reply KEY: {}", key);
 
-        switch (key) {
+        // 레코드에서 record.value()를 DTO 타입으로 캐스팅하여 사용할 것
+
+//        ModelMapper mapper = new ModelMapper();
+//        TransferRequest request = mapper.map(record.value(), TransferRequest.class);
+//        TransferResponse response = processTransferInBank(request);
+//        if (cmd == null) return response;
+//        switch (cmd) {
+//            case CommunicationProtocol.SEND_TEST_MESSAGE:
+//                log.info("Called SEND_TEST_MESSAGE!");
+//                break;
+//            case CommunicationProtocol.REQUEST_DEPOSIT:
+//                log.info("Called REQUEST_DEPOSIT!");
+//                break;
+//            case CommunicationProtocol.REQUEST_WITHDRAW:
+//                log.info("Called REQUEST_WITHDRAW!");
+//                break;
+//        }
+//
+//        log.info("Transfer processed, sending response: {}", response);
+
+        Object response = null;
+        return response;
+    }
+
+    /**
+     * 단방향 메세지 요청에 대한 카프카 리스너
+     * (kafkaListenerUnidirectionalContainerFactory 사용)
+     *
+     * @param cmd    통신 프로토콜
+     * @param record 레코드
+     */
+    @KafkaListener(topics = "${spring.kafka.push-topic}", containerFactory = "kafkaListenerUnidirectionalContainerFactory")
+    public void receiveMessage(@Header(value = "CMD", required = false) String cmd,
+                               ConsumerRecord<String, Object> record) {
+        log.info("Received unidirectional message in bank service: {}", record.value());
+        log.info("Received CMD: {}", cmd);
+
+        if (cmd == null) return;
+        switch (cmd) {
+            // 로그 확인
             case CommunicationProtocol.SEND_TEST_MESSAGE:
                 log.info("Called SEND_TEST_MESSAGE!");
                 break;
@@ -69,35 +109,6 @@ public class TransferServiceImpl implements TransferService{
                 log.info("Called REQUEST_WITHDRAW!");
                 break;
         }
-
-        return new Object(); //Resonse DTO를 응답으로 보내야됨 (수정 필요)
-    }
-
-    /**
-     * 단방향 메세지 요청에 대한 카프카 리스너
-     * (kafkaListenerUnidirectionalContainerFactory 사용)
-     *
-     * @param key   식별자 키
-     * @param value DTO 객체
-     */
-    @KafkaListener(topics = "${spring.kafka.push-topic}", containerFactory = "kafkaListenerUnidirectionalContainerFactory")
-    public void receiveMessage(@Header(KafkaHeaders.RECEIVED_KEY) String key, Object value) {
-        log.info("Received unidirectional message in bank service: {}", value);
-        log.info("Received KEY: {}", key);
-
-        switch (key) {
-            // 실제 은행 송금 처리 로직 구현 (여기서는 간단히 시뮬레이션)
-            case CommunicationProtocol.SEND_TEST_MESSAGE:
-                log.info("Hello World!");
-                break;
-            case CommunicationProtocol.REQUEST_DEPOSIT:
-                log.info("Hello World!!");
-                break;
-            case CommunicationProtocol.REQUEST_WITHDRAW:
-                log.info("Hello World!!!");
-                break;
-        }
-
     }
 
     // 출금 이체
